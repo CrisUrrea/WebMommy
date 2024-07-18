51d990aab0129f0624968f027332dbd9c5f50511
Cristhian Urrea <100173244+CrisUrrea@users.noreply.github.com>
2024-07-17 22:56:29 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,17 +5,17 @@ let future = document.getElementById('future');
 const redireccion = () => {
     past.addEventListener('click', (e) => {
         e.preventDefault();
-        location.replace('../templates/past.html');
+        location.replace('./templates/past.html');
     });
 
     present.addEventListener('click', (e) => {
         e.preventDefault();
-        location.replace('../templates/present.html');
+        location.replace('./templates/present.html');
     });
 
     future.addEventListener('click', (e) => {
         e.preventDefault();
-        location.replace('../templates/future.html');
+        location.replace('./templates/future.html');
     });
 }
 
